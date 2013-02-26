8a7573d9c423d5c8899061e05767abcb48e1537a
rupertheilmeier <rupert_heilmeier@web.de>
2013-02-26 19:39:01 +0100

    Xml schon geupdatet

--- Literator/gen/de/projekt/literator/R.java
+++ Literator/gen/de/projekt/literator/R.java
@@ -15,10 +15,18 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
+        public static final int actionbarFunk1=0x7f070009;
+        public static final int actionbarFunk2=0x7f07000b;
+        public static final int actionbarHome=0x7f070006;
+        public static final int actionbarTxt=0x7f070008;
+        public static final int actionbar_seperator_funk=0x7f07000a;
         public static final int button=0x7f070001;
+        public static final int buttonlayout=0x7f070004;
+        public static final int buttonlayout2=0x7f070005;
         public static final int editText=0x7f070003;
         public static final int headerbar=0x7f070000;
-        public static final int menu_settings=0x7f070004;
+        public static final int linearLayout1=0x7f070007;
+        public static final int menu_settings=0x7f07000c;
         public static final int textView=0x7f070002;
     }
     public static final class layout {
