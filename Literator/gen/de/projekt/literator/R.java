1806741ffe5625198fbdb23cf8f08cd762a5ad11
rupertheilmeier <rupert_heilmeier@web.de>
2013-03-01 19:37:07 +0100

    Angepasstes Iphonestyle Layout

--- Literator/gen/de/projekt/literator/R.java
+++ Literator/gen/de/projekt/literator/R.java
@@ -10,35 +10,58 @@ package de.projekt.literator;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int tabDark=0x7f040001;
+        public static final int tabMedium=0x7f040000;
+        public static final int tabTransparent=0x7f040002;
+    }
+    public static final class dimen {
+        public static final int corner_radius=0x7f050000;
+        public static final int corner_radius_zero=0x7f050001;
+        public static final int tab_space=0x7f050003;
+        public static final int tab_space_bottom_line=0x7f050005;
+        public static final int tab_space_plus1=0x7f050004;
+        public static final int tab_space_top=0x7f050002;
+        public static final int tab_space_unselected_top=0x7f050006;
+    }
     public static final class drawable {
-        public static final int header_bar=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int icon_bibliograph_list_tab=0x7f020002;
-        public static final int icon_books_tab=0x7f020003;
-        public static final int icon_mail_tab=0x7f020004;
-        public static final int icon_prefrences_tab=0x7f020005;
-        public static final int icon_scan_tab=0x7f020006;
-        public static final int menue_bar_layer1=0x7f020007;
-        public static final int normal_bibliograph=0x7f020008;
-        public static final int normal_mail=0x7f020009;
-        public static final int normal_prefrences=0x7f02000a;
-        public static final int normal_scanned_books=0x7f02000b;
-        public static final int roll_over_bibliograph=0x7f02000c;
-        public static final int roll_over_mail=0x7f02000d;
-        public static final int roll_over_prefrences=0x7f02000e;
-        public static final int roll_over_scanned_books=0x7f02000f;
-        public static final int scan_isbn=0x7f020010;
+        public static final int app_background=0x7f020000;
+        public static final int header_bar=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int icon_bibliograph_list_tab=0x7f020004;
+        public static final int icon_books_tab=0x7f020005;
+        public static final int icon_mail_tab=0x7f020006;
+        public static final int icon_prefrences_tab=0x7f020007;
+        public static final int icon_scan_tab=0x7f020008;
+        public static final int menue_bar_layer1=0x7f020009;
+        public static final int normal_bibliograph=0x7f02000a;
+        public static final int normal_mail=0x7f02000b;
+        public static final int normal_prefrences=0x7f02000c;
+        public static final int normal_scanned_books=0x7f02000d;
+        public static final int roll_over_bibliograph=0x7f02000e;
+        public static final int roll_over_mail=0x7f02000f;
+        public static final int roll_over_prefrences=0x7f020010;
+        public static final int roll_over_scanned_books=0x7f020011;
+        public static final int scan_isbn=0x7f020012;
+        public static final int scanned_books_ro=0x7f020013;
+        public static final int tab_focus=0x7f020014;
+        public static final int tab_indicator=0x7f020015;
+        public static final int tab_press=0x7f020016;
+        public static final int tab_selected=0x7f020017;
+        public static final int tab_unselected=0x7f020018;
     }
     public static final class id {
-        public static final int bibTest=0x7f070000;
-        public static final int bookTest=0x7f070001;
-        public static final int edittext=0x7f070002;
-        public static final int header_bar=0x7f070004;
-        public static final int mailTest=0x7f070003;
-        public static final int menu_settings=0x7f070008;
-        public static final int menue_bar_layer1=0x7f070005;
-        public static final int prefTest=0x7f070006;
-        public static final int scanTest=0x7f070007;
+        public static final int bibTest=0x7f090000;
+        public static final int bookTest=0x7f090001;
+        public static final int edittext=0x7f090002;
+        public static final int header_bar=0x7f090004;
+        public static final int icon=0x7f090008;
+        public static final int mailTest=0x7f090003;
+        public static final int menu_settings=0x7f090009;
+        public static final int menue_bar_layer1=0x7f090005;
+        public static final int prefTest=0x7f090006;
+        public static final int scanTest=0x7f090007;
     }
     public static final class layout {
         public static final int activity_bibligraph_list=0x7f030000;
@@ -48,21 +71,22 @@ public final class R {
         public static final int activity_main=0x7f030004;
         public static final int activity_prefrences=0x7f030005;
         public static final int activity_scan=0x7f030006;
+        public static final int tab_indicator=0x7f030007;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f080000;
     }
     public static final class string {
-        public static final int ISBN=0x7f040006;
-        public static final int app_name=0x7f040000;
-        public static final int button=0x7f040004;
-        public static final int editText=0x7f040003;
-        public static final int header_bar=0x7f040007;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int menue_bar_layer1=0x7f040008;
-        public static final int tab_button=0x7f040009;
-        public static final int textView=0x7f040005;
+        public static final int ISBN=0x7f060006;
+        public static final int app_name=0x7f060000;
+        public static final int button=0x7f060004;
+        public static final int editText=0x7f060003;
+        public static final int header_bar=0x7f060007;
+        public static final int hello_world=0x7f060001;
+        public static final int menu_settings=0x7f060002;
+        public static final int menue_bar_layer1=0x7f060008;
+        public static final int tab_button=0x7f060009;
+        public static final int textView=0x7f060005;
     }
     public static final class style {
         /** 
@@ -86,10 +110,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- Literator/src/de/projekt/literator/StartActivity.java
+++ Literator/src/de/projekt/literator/StartActivity.java
@@ -4,15 +4,19 @@ import de.projekt.literator.scanner.ScanActivity;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
+
 
 
 public class StartActivity extends TabActivity {
 
 	
-	
+	private TabHost tabHost;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,39 +25,60 @@ public class StartActivity extends TabActivity {
 	}
 
 	private void setupTabUI() {
-		TabHost tabHost = getTabHost();
-		
-		TabSpec booksspec = tabHost.newTabSpec("books");
-		booksspec.setIndicator(null, getResources().getDrawable(R.drawable.icon_books_tab));
-        Intent booksIntent = new Intent(this, BooksActivity.class);
-        booksspec.setContent(booksIntent);
-        
-        TabSpec bibliographList = tabHost.newTabSpec("lists");
-		bibliographList.setIndicator(null, getResources().getDrawable(R.drawable.icon_bibliograph_list_tab));
-        Intent bibliographIntent = new Intent(this, ListsActivity.class);
-        bibliographList.setContent(bibliographIntent);
-        
-        TabSpec scanspec = tabHost.newTabSpec("scan");
-		scanspec.setIndicator(null, getResources().getDrawable(R.drawable.icon_scan_tab));
-        Intent scanIntent = new Intent(this, ScanActivity.class);
-        scanspec.setContent(scanIntent);
-        
-        TabSpec mailspec = tabHost.newTabSpec("mail");
-		mailspec.setIndicator(null, getResources().getDrawable(R.drawable.icon_mail_tab));
-        Intent mailIntent = new Intent(this, MailActivity.class);
-        mailspec.setContent(mailIntent);
-        
-        TabSpec prefrencesspec = tabHost.newTabSpec("prefrences");
-		prefrencesspec.setIndicator(null, getResources().getDrawable(R.drawable.icon_prefrences_tab));
-        Intent prefrencesIntent = new Intent(this, ListsActivity.class);
-        prefrencesspec.setContent(prefrencesIntent);
-        
-        tabHost.addTab(booksspec);
-        tabHost.addTab(bibliographList);
-        tabHost.addTab(scanspec);
-        tabHost.addTab(mailspec);
-        tabHost.addTab(prefrencesspec);
+		tabHost = getTabHost();
+		setTabs();
 	}
+	private void setTabs()
+	{
+		addTab("scanned_books", R.drawable.icon_books_tab, BooksActivity.class);
+		addTab("list", R.drawable.icon_bibliograph_list_tab, ListsActivity.class);
+		addTab("Fake", R.drawable.icon_books_tab, BooksActivity.class);
+		addTab("Mail", R.drawable.icon_mail_tab, MailActivity.class);
+		addTab("Prefrences", R.drawable.icon_prefrences_tab, PrefrencesActivity.class);
+	}
+	private void addTab(String labelId, int drawableId, Class<?> c)
+	{
+		Intent intent = new Intent(this, c);
+		TabHost.TabSpec spec = tabHost.newTabSpec("tab" + labelId);	
+		
+		View tabIndicator = LayoutInflater.from(this).inflate(R.layout.tab_indicator, getTabWidget(), false);
+		ImageView icon = (ImageView) tabIndicator.findViewById(R.id.icon);
+		icon.setImageResource(drawableId);		
+		spec.setIndicator(tabIndicator);
+		spec.setContent(intent);
+		tabHost.addTab(spec);
+	}
+//		TabSpec booksspec = tabHost.newTabSpec("books");
+//		booksspec.setIndicator(null, getResources().getDrawable(R.drawable.icon_books_tab));
+//        Intent booksIntent = new Intent(this, BooksActivity.class);
+//        booksspec.setContent(booksIntent);
+//        
+//        TabSpec bibliographList = tabHost.newTabSpec("lists");
+//		bibliographList.setIndicator(null, getResources().getDrawable(R.drawable.icon_bibliograph_list_tab));
+//        Intent bibliographIntent = new Intent(this, ListsActivity.class);
+//        bibliographList.setContent(bibliographIntent);
+//        
+//        TabSpec scanspec = tabHost.newTabSpec("scan");
+//		scanspec.setIndicator(null, getResources().getDrawable(R.drawable.icon_scan_tab));
+//        Intent scanIntent = new Intent(this, ScanActivity.class);
+//        scanspec.setContent(scanIntent);
+//        
+//        TabSpec mailspec = tabHost.newTabSpec("mail");
+//		mailspec.setIndicator(null, getResources().getDrawable(R.drawable.icon_mail_tab));
+//        Intent mailIntent = new Intent(this, MailActivity.class);
+//        mailspec.setContent(mailIntent);
+//        
+//        TabSpec prefrencesspec = tabHost.newTabSpec("prefrences");
+//		prefrencesspec.setIndicator(null, getResources().getDrawable(R.drawable.icon_prefrences_tab));
+//        Intent prefrencesIntent = new Intent(this, ListsActivity.class);
+//        prefrencesspec.setContent(prefrencesIntent);
+//        
+//        tabHost.addTab(booksspec);
+//        tabHost.addTab(bibliographList);
+//        tabHost.addTab(scanspec);
+//        tabHost.addTab(mailspec);
+//        tabHost.addTab(prefrencesspec);
+//	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
